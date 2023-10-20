eb1a4077ac879cb4e79fee63cdd17abc66438cf8
JAMSHEDWEBDEV <jamsheddc60@gmail.com>
2023-10-20 19:11:55 +0600

    added delete operation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -42,6 +42,13 @@ async function run() {
       res.send(result);
     })
     
+    // data delete operation start 
+    app.delete('/products/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await groceryCollection.deleteOne(query);
+        res.send(result);
+    })
 
     
     // Send a ping to confirm a successful connection
